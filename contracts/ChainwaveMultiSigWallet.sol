4ca8f16e6deeda07d84765e38f15ce925799ece1
Benjamin M. Brown <benjaminbb@gmail.com>
2020-09-28 12:55:45 +0000

    initial truffle setup

---
+++ contracts/ChainwaveMultiSigWallet.sol
@@ -0,0 +1,78 @@
+pragma solidity ^0.6.0;
+pragma experimental ABIEncoderV2;
+
+contract ChainwaveMultiSigWallet {
+    
+    address[] public approvers;
+    uint public quorum;
+    
+    struct Transfer{
+        uint id;
+        uint amount;
+        address payable to;
+        uint approvals;
+        bool sent;
+    }
+    
+    Transfer[] public transfers;
+    
+    mapping(address => mapping(uint => bool)) public approvals;
+    
+    constructor(address[] memory _approvers, uint _quorum) public {
+        approvers = _approvers;
+        quorum = _quorum;
+    }
+    
+    function getApprovers() external view returns (address[] memory){
+        return approvers;
+    }
+    
+     function getTransfers() external view returns (Transfer[] memory){
+        return transfers;
+    }
+    
+
+    function createTransfer(uint amount, address payable to) external onlyApprover {
+         transfers.push(
+             Transfer(
+              transfers.length,
+              amount,
+              to,
+              0,
+              false
+            ));
+    }
+    
+    
+   function approveTransfer(uint id) external{
+       require(transfers[id].sent == false, "Error: Transfer has already been sent");
+       require(approvals[msg.sender][id] == false, "Error: Cannot approve a trasnfer twice");
+       
+       approvals[msg.sender][id] == true;
+       transfers[id].approvals ++;
+       
+       if(transfers[id].approvals >= quorum){
+           transfers[id].sent = true;
+           address payable to = transfers[id].to;
+           uint amount = transfers[id].amount;
+           to.transfer(amount);
+       }
+   } 
+   
+   ///NATIVE RECEIPT OF FUNDS
+   
+   receive() external payable {}
+    
+   modifier onlyApprover(){
+       bool allowed = false;
+       for(uint i = 0; i < approvers.length; i++){
+           if(approvers[i] == msg.sender){
+               allowed = true;
+           }
+       }
+       require(allowed == true);
+       _;
+   }
+
+    
+}
