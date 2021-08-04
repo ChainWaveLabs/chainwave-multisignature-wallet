c3734f5c96b859c5da54fb90759cc95b179469c1
benjaminmbrown <benjaminbb@gmail.com>
2021-08-03 23:23:30 -0500

    update for local env + demo

--- README.md
+++ README.md
@@ -6,4 +6,12 @@ Details: https://chainwave.io/multi-signature-ethereum-wallet/
 ![](multi-sig-2.png)
 ![](multi-sig-3.png)
 
+## Deploy to Kovan
+
 `truffle migrate --reset --network kovan`
+
+\*\* Deploy Local Blockchain
+
+`truffle develop`
+
+`migrate --reset`
--- client/src/web3utils.js
+++ client/src/web3utils.js
@@ -2,39 +2,40 @@ import Web3 from 'web3';
 import ChainwaveMultiSigWallet from './contracts/ChainwaveMultiSigWallet.json';
 
 const getWeb3 = () => {
-    //  return new Web3('http://localhost:9545');
+  //FOR LOCAL
+  return new Web3('http://localhost:9545');
 
-    return new Promise((resolve, reject) => {
-        window.addEventListener('load', async () => {
-            if (window.ethereum) {
-                const web3 = new Web3(window.ethereum);
+  //FOR TESTNET & LIVE
+  // return new Promise((resolve, reject) => {
+  //     window.addEventListener('load', async () => {
+  //         if (window.ethereum) {
+  //             const web3 = new Web3(window.ethereum);
 
-                try {
-                    await window.ethereum.enable();
-                    resolve(web3);
-                } catch (error) {
-                    reject(error);
-                }
-            } else if (window.web3) {
-                resolve(window.web3);
-            } else {
-                reject("Must install metamask or other compatible wallet");
-            }
-        })
+  //             try {
+  //                 await window.ethereum.enable();
+  //                 resolve(web3);
+  //             } catch (error) {
+  //                 reject(error);
+  //             }
+  //         } else if (window.web3) {
+  //             resolve(window.web3);
+  //         } else {
+  //             reject("Must install metamask or other compatible wallet");
+  //         }
+  //     })
 
-    })
+  // })
 };
 
-const getWallet = async web3 => {
+const getWallet = async (web3) => {
+  const networkId = await web3.eth.net.getId();
 
-    const networkId = await web3.eth.net.getId();
+  const deployedNetwork = ChainwaveMultiSigWallet.networks[networkId];
 
-    const deployedNetwork = ChainwaveMultiSigWallet.networks[networkId];
+  return new web3.eth.Contract(
+    ChainwaveMultiSigWallet.abi,
+    deployedNetwork && deployedNetwork.address
+  );
+};
 
-    return new web3.eth.Contract(
-        ChainwaveMultiSigWallet.abi,
-        deployedNetwork && deployedNetwork.address
-    );
-}
-
-export { getWeb3, getWallet }
+export { getWeb3, getWallet };
